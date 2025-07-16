c0bfcd8771e1a3ad8d057a7b02ed1cdeebd6d394
hanisha25 <hanishayadav13@gmail.com>
2025-07-16 21:46:09 +0530

    adding example

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ function displayData(data){
     const wordData = data[0];
     let synonyms = [];
     let antonyms = [];
+    let examples = [];
 
     wordData.meanings.forEach(meaning => {
         if (meaning.synonyms && meaning.synonyms.length > 0) {
@@ -29,7 +30,6 @@ function displayData(data){
         if (meaning.antonyms && meaning.antonyms.length > 0) {
             antonyms = antonyms.concat(meaning.antonyms);
         }
-        // Also check definitions for synonyms/antonyms
         meaning.definitions.forEach(def => {
             if (def.synonyms && def.synonyms.length > 0) {
                 synonyms = synonyms.concat(def.synonyms);
@@ -37,13 +37,18 @@ function displayData(data){
             if (def.antonyms && def.antonyms.length > 0) {
                 antonyms = antonyms.concat(def.antonyms);
             }
+            if (def.example) {
+                examples.push(def.example);
+            }
         });
     });
 
     // Remove duplicates
     synonyms = [...new Set(synonyms)];
     antonyms = [...new Set(antonyms)];
+    examples = [...new Set(examples)];
 
     document.getElementById("synonyms").textContent = "Synonyms: " + (synonyms.length ? synonyms.join(", ") : "None");
     document.getElementById("antonyms").textContent = "Antonyms: " + (antonyms.length ? antonyms.join(", ") : "None");
+    document.getElementById("examples").textContent = "Examples: " + (examples.length ? examples.join(" | ") : "None");
 }
